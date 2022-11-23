cdf7085e966a52d6d938bea45ff0d198b1faf9ee
11点多就睡觉 <3169105265@qq.com>
2022-11-23 20:30:12 +0800

    homework02

--- React/helloreact/src/TodoApp/index.js
+++ React/helloreact/src/TodoApp/index.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import TodoList from '../TodoList';
+import './index.css';
 export default class TodoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
+      items: [
+        {
+          text: "one",
+          id: "001",
+          checked: true
+        },
+        {
+          text: "two",
+          id: "002",
+          checked: false
+        }
+      ],
       text: '',
+      checkedAll: false,
     }
   }
   render() {
     return (
-      <div style={{padding:"10 0 0 30"}}>
+      <div className="App-main">
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
+        <TodoList items={this.state.items} deleteTodo={this.deleteTodo} handleChanged={this.handleChanged}/>
+        <input className='ip1' type="checkbox" defaultChecked={this.state.checkedAll} onChange={() => this.AllTodo()}></input>全部完成
+          <button onClick={() => this.deleteChecked()} className='add'>
+            删除已完成
+          </button>
+        <form className='form' onSubmit={this.handleSubmit}>
+          <br/>
           <label htmlFor="new-todo">
             What needs to be done?
           </label>
@@ -23,8 +41,7 @@ export default class TodoApp extends React.Component {
             onChange={this.handleChange}
             value={this.state.text}
           />
-          <br />
-          <button style={{marginTop:"10px"}}>
+          <button className='add'>
             Add #{this.state.items.length + 1}
           </button>
         </form>
@@ -32,10 +49,53 @@ export default class TodoApp extends React.Component {
     );
   }
 
-  handleChange = (e) => {
-    this.setState({ text: e.target.value });
+  //修改单个TODO的checked
+  handleChanged = (id,checked) => {
+    this.setState(state => ({
+      items: state.items.map((todo) => {
+        if (todo.id === id) return {...todo, checked:!checked}
+        else return todo
+      })
+    }));
   }
 
+  //删除已完成
+  deleteChecked = () => {
+    this.setState(state => ({
+      items: state.items.filter((item) => {
+        return item.checked !== true
+      })
+    }));
+  }
+
+  //完成全部
+  AllTodo = () => {
+    let checked = !this.state.checkedAll;
+    console.log(checked);
+    this.setState(state => ({
+      items: state.items.map((item) => {
+        return {...item,checked:checked}
+      }),
+      checkedAll: checked
+    }));
+  }
+
+
+  //删除选项
+  deleteTodo = (id) => {
+    this.setState(state => ({
+      items: state.items.filter((item) => {
+        return item.id !== id
+      })
+    }));
+  }
+
+  //input框输入变化
+  handleChange = (e) => {
+    this.setState({ text: e.target.value.trim() });
+  }
+
+  //按钮事件
   handleSubmit = (e) => {
     e.preventDefault();
     if (this.state.text.length === 0) {
@@ -43,11 +103,12 @@ export default class TodoApp extends React.Component {
     }
     const newItem = {
       text: this.state.text,
-      id: Date.now()
+      id: Date.now(),
+      checked: false
     };
     this.setState(state => ({
       items: state.items.concat(newItem),
-      text: ''
+      text: '',
     }));
   }
 }
--- React/helloreact/src/TodoList/index.js
+++ React/helloreact/src/TodoList/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
 export default class TodoList extends React.Component {
 	constructor(props) {
 		super(props)
@@ -10,9 +11,22 @@ export default class TodoList extends React.Component {
 		return (
 		<ul>
 			{this.props.items.map(item => (
-			<li key={item.id}>{item.text}</li>
+			<li className='list-li' key={item.id}>
+				<input type="checkbox" checked={item.checked} onChange={() => {this.handleChange(item.id,item.checked)}}></input>
+				{item.text}
+				<button onClick={() => {this.handleDelete(item.id)}}>Delete</button>	
+			</li>
 			))}
 		</ul>
 		);
   	}
+
+	handleChange = (id,checked) => {
+		console.log(this.props);
+		this.props.handleChanged(id,checked);
+	}
+
+	handleDelete = (id) => {
+		this.props.deleteTodo(id);
+	}
 }
